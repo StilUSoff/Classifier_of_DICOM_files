a86b7e9a09cfe9ca3c0f07d88a52fe000dd04d14
Tugay Vadim <stilusoff@gmail.com>
2023-09-06 09:01:22 +0300

    docker start

---
+++ bin/classifier/run_for_docker.py
@@ -0,0 +1,5 @@
+from classifier import main
+
+work_folder,checkpoint,attributes_file,device = input("work_folder","checkpoint","attributes_file","device")
+
+main(work_folder,checkpoint,attributes_file,device)
