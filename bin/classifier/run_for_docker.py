d1c3810c4009e609fc03b6e5572070c9fa0cd911
stilusoff <tugay.vadim@gmail.com>
2023-09-07 20:18:18 +0300

    readme changes

--- bin/classifier/run_for_docker.py
+++
@@ -1,5 +0,0 @@
-from classifier import main
-
-work_folder,checkpoint,attributes_file,device = input("work_folder","checkpoint","attributes_file","device")
-
-main(work_folder,checkpoint,attributes_file,device)
